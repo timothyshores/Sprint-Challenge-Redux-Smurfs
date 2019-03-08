72fca9a4cbc0969fec7193f8887649c997540fa7
Timothy Shores <timothymshores@gmail.com>
2019-03-08 12:05:45 -0500

    Export rootReducer, create get and add actions and fix rootReducer import

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -13,3 +13,47 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+
+import axios from 'axios'
+
+export const GET_SMURFS = 'GET_SMURFS'
+export const GET_SMURFS_SUCCESS = 'GET_SMURFS_SUCCESS'
+export const GET_SMURFS_ERROR = 'GET_SMURFS_ERROR'
+
+export const ADD_SMURF = 'ADD_SMURF'
+export const ADD_SMURF_SUCCESS = 'ADD_SMURF_SUCCESS'
+export const ADD_SMURF_ERROR = 'ADD_SMURF_ERROR'
+
+export const getSmurfs = () => {
+  return dispatch => {
+    dispatch({ type: GET_SMURFS })
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(response => {
+        dispatch({ type: GET_SMURFS_SUCCESS, payload: response.data })
+      })
+      .catch(error => {
+        console.log(error)
+        dispatch({ type: GET_SMURFS_ERROR })
+      })
+  }
+}
+
+export const addSmurf = smurf => {
+  return dispatch => {
+    dispatch({ type: ADD_SMURF })
+    axios
+      .post('http://localhost:3333/smurfs', smurf)
+      .then(response =>
+        dispatch({
+          type: ADD_SMURF_SUCCESS,
+          payload: response.data,
+        })
+      )
+      .catch(error => {
+        console.log(error)
+        dispatch({ type: ADD_SMURF_ERROR })
+      })
+  }
+}
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,6 +1,19 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
+// Import all action types from `../actions`
+
+import {
+  GET_SMURFS,
+  GET_SMURFS_SUCCESS,
+  GET_SMURFS_ERROR,
+  ADD_SMURF,
+  ADD_SMURF_SUCCESS,
+  ADD_SMURF_ERROR,
+  DELETE_SMURF,
+  DELETE_SMURF_SUCCESS,
+  DELETE_SMURF_ERROR,
+  UPDATE_SMURF,
+  UPDATE_SMURF_SUCCESS,
+  UPDATE_SMURF_ERROR,
+} from '../actions/index'
 
 // Initial/default state for this projec
 
@@ -21,3 +34,41 @@ const initialState = {
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
 
+export const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_SMURFS:
+      return {
+        ...state,
+        gettingSmurfs: true,
+      }
+    case GET_SMURFS_SUCCESS:
+      return {
+        ...state,
+        gettingSmurfs: false,
+        smurfs: [...state, ...action.payload],
+      }
+    case GET_SMURFS_ERROR:
+      return {
+        ...state,
+        gettingSmurfs: false,
+      }
+    case ADD_SMURF:
+      return {
+        ...state,
+        addingSmurf: true,
+      }
+    case ADD_SMURF_SUCCESS:
+      return {
+        ...state,
+        addingSmurf: false,
+        smurfs: [...action.payload],
+      }
+    case ADD_SMURF_ERROR:
+      return {
+        ...state,
+        addingSmurf: false,
+      }
+    default:
+      return state
+  }
+}
